7b090fa0c27ecea090d74db628318fc9a50e403e
souvik500 <120314952+souvik500@users.noreply.github.com>
2023-11-14 02:03:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,27 @@
 //your JS code here. If required.
-let textn=document.getElementById("text");
-let numbert=document.getElementById("delay");
-let button=document.getElementById("btn");
-let divd=document.getElementById("output");
- 
-async function displayf(textv,delay) {
-	try{
-		 if (!textv || isNaN(delay) || delay < 0) {
-                divd.textContent = "Please enter valid text and delay.";
-                return;
-            }
-		await new Promise((resolve)=>{
-			setTimeout(()=>{
-				divd.textContent=textv;
-			},delay);
-		})
-	}
-	catch{
-		
+
+ let output =document.getElementById("output");
+let form = document.getElementById("btn");
+  let msgi =document.getElementById("text");
+let delayi =document.getElementById("delay");
+form.addEventListener("click",(e)=>{
+	e.preventDefault();
+ const	msg= msgi.value;
+	const delay=delayi.value;
+	if(msg && delay>0)
+	{
+		main(delay,msg);
 	}
 	
-}
-button.addEventListener("click",(e)=>{
-	e.preventDefault();
-	let textv=textn.value;
-	let delay=parseInt(numbert.value)*1000;
-	displayf(textv,delay);
+	
 })
+  
+async function main( delay,msg){
+
+	  await new Promise((resolve)=>{
+		  setTimeout(()=>{
+			  resolve();
+		  },delay);
+	  })
+	 output.innerText = msg; 
+}
